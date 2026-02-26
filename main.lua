a21193be71f118886b490e1dc6e80681aa14fc8c
spaceyounoob <lawrencearcena6@gmail.com>
2026-02-26 16:58:41 +0800

    made him do a cool spin :)

--- main.lua
+++ main.lua
@@ -5,13 +5,20 @@ local shader = love.graphics.newShader("g3d/g3d.vert", "content/shader/lighting.
 local maxwell = g3d.newModel("content/models/dingus.obj", "content/textures/dingus.jpg", {10,5,6}, nil, {1,1,1})
 local maxwellwhiskers = g3d.newModel("content/models/whiskers.obj", "content/textures/dingus-whiskers.png", {10,5,6}, nil, {1,1,1})
 eras = love.graphics.newFont("content/fonts/erasdemi.ttf", 30)
+dingustheme = love.sound.newSoundData("content/music/maxwell.mp3")
+song = love.audio.newSource(dingustheme, static)
+song:setLooping(true)
+song:play()
 
 function love.update(dt)
     timer = timer + dt
+maxwell:setRotation(0, 0, math.pi - timer)
+maxwellwhiskers:setRotation(0, 0, math.pi - timer)
     g3d.camera.firstPersonMovement(dt)
     if love.keyboard.isDown "escape" then
-        love.event.push "quit"
-    end
+        love.event.push "quit"    
+song:stop()
+end
 end
 
 function love.draw()
@@ -24,6 +31,6 @@ love.graphics.print("FPS: "..tostring(love.timer.getFPS( )), 10, 40)
    love.graphics.print("Time: "..tostring(love.timer.getTime( )), 10, 70)
 end
 
-function love.mousemoved(x,y, dx,dy)
+ function love.mousemoved(x,y, dx,dy)
     g3d.camera.firstPersonLook(dx,dy)
-end
+ end
