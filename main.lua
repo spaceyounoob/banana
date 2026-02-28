da569b9f484c108c771d28c35d0d3078ef5d41ec
spaceyounoob <lawrencearcena6@gmail.com>
2026-02-28 15:27:20 +0800

    last commit b4 i go back to windows

--- main.lua
+++ main.lua
@@ -1,7 +1,7 @@
 local g3d = require "g3d"
 local skybox = g3d.newModel("content/models/sphere.obj", "content/sky/plainsky.png", nil, nil, 500)
 local timer = 0
-local shader = love.graphics.newShader("g3d/g3d.vert", "content/shader/lighting.frag")
+local shadows = love.graphics.newShader("g3d/g3d.vert", "content/shader/lighting.frag")
 local maxwell = g3d.newModel("content/models/dingus.obj", "content/textures/dingus.jpg", {10,5,6}, nil, {1,1,1})
 local maxwellwhiskers = g3d.newModel("content/models/whiskers.obj", "content/textures/dingus-whiskers.png", {10,5,6}, nil, {1,1,1})
 eras = love.graphics.newFont("content/fonts/erasdemi.ttf", 30)
@@ -22,6 +22,8 @@ end
 end
 
 function love.draw()
+   g3d.camera.updateViewMatrix(shadows)
+    g3d.camera.updateProjectionMatrix(shadows)
     skybox:draw()
     maxwell:draw()
     maxwellwhiskers:draw()
